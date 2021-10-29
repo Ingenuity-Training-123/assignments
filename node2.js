72f942f5648f75245903c3a559ee70029248364a
Kanak-Saini <92813684+Kanak-Saini@users.noreply.github.com>
2021-10-30 03:53:16 +0530

    Update node2.js

--- node2.js
+++ node2.js
@@ -20,11 +20,11 @@ let autoplay = 0;
 let index_no = 0;
 let Playing_song = false;
 
-//create a audio Element
+
 let track = document.createElement('audio');
 
 
-//All songs list
+
 let All_song = [
    {
      name: "Levitating",
@@ -59,10 +59,10 @@ let All_song = [
 ];
 
 
-// All functions
 
 
-// function load the track
+
+
 function load_track(index_no){
 	clearInterval(timer);
 	reset_slider();
@@ -89,7 +89,7 @@ function mute_sound(){
 }
 
 
-// checking.. the song is playing or not
+
  function justplay(){
  	if(Playing_song==false){
  		playsong();
@@ -100,19 +100,19 @@ function mute_sound(){
  }
 
 
-// reset song slider
+
  function reset_slider(){
  	slider.value = 0;
  }
 
-// play song
+
 function playsong(){
   track.play();
   Playing_song = true;
   play.innerHTML = '<i class="fa fa-pause" aria-hidden="true"></i>';
 }
 
-//pause song
+
 function pausesong(){
 	track.pause();
 	Playing_song = false;
@@ -120,7 +120,7 @@ function pausesong(){
 }
 
 
-// next song
+
 function next_song(){
 	if(index_no < All_song.length - 1){
 		index_no += 1;
@@ -135,7 +135,7 @@ function next_song(){
 }
 
 
-// previous song
+
 function previous_song(){
 	if(index_no > 0){
 		index_no -= 1;
@@ -150,19 +150,19 @@ function previous_song(){
 }
 
 
-// change volume
+
 function volume_change(){
 	volume_show.innerHTML = recent_volume.value;
 	track.volume = recent_volume.value / 100;
 }
 
-// change slider position 
+
 function change_duration(){
 	slider_position = track.duration * (slider.value / 100);
 	track.currentTime = slider_position;
 }
 
-// autoplay function
+
 function autoplay_switch(){
 	if (autoplay==1){
        autoplay = 0;
@@ -177,14 +177,14 @@ function autoplay_switch(){
 function range_slider(){
 	let position = 0;
         
-        // update slider position
+       
 		if(!isNaN(track.duration)){
 		   position = track.currentTime * (100 / track.duration);
 		   slider.value =  position;
 	      }
 
        
-       // function will run when the song is over
+     
        if(track.ended){
        	 play.innerHTML = '<i class="fa fa-play" aria-hidden="true"></i>';
            if(autoplay==1){
