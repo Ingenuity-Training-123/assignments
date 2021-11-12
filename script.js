a7cd76c51ef199cc250ae60d9a9cb3612e880741
Kanak-Saini <92813684+Kanak-Saini@users.noreply.github.com>
2021-11-13 05:04:08 +0530

    changes

--- script.js
+++ script.js
@@ -1,88 +1,154 @@
-let canvas = document.getElementById("canvas");
-canvas.width = window.innerWidth - 60;
-canvas.height = window.innerHeight * 0.8;
-let context = canvas.getContext("2d");
-context.fillStyle = "white";
-context.fillRect(0, 0, canvas.width, canvas.height);
-let restore_array = [];
-let start_index = -1;
-let stroke_color = 'black';
-let stroke_width = "2";
-let is_drawing = false;
+var canvas,
+    context,
+    dragging = false,
+    dragStartLocation,
+    snapshot;
 
-function change_color(element) {
-  stroke_color = element.style.background;
+
+function getCanvasCoordinates(event) {
+    var x = event.clientX - canvas.getBoundingClientRect().left,
+        y = event.clientY - canvas.getBoundingClientRect().top;
+
+    return {x: x, y: y};
 }
 
-function change_width(element) {
-  stroke_width = element.innerHTML
+function takeSnapshot() {
+    snapshot = context.getImageData(0, 0, canvas.width, canvas.height);
 }
 
-function start(event) {
-  is_drawing = true;
-  context.beginPath();
-  context.moveTo(getX(event), getY(event));
-  event.preventDefault();
+function restoreSnapshot() {
+    context.putImageData(snapshot, 0, 0);
 }
 
-function draw(event) {
-  if (is_drawing) {
-    context.lineTo(getX(event), getY(event));
-    context.strokeStyle = stroke_color;
-    context.lineWidth = stroke_width;
-    context.lineCap = "round";
-    context.lineJoin = "round";
+function drawLine(position) {
+    context.beginPath();
+    context.moveTo(dragStartLocation.x, dragStartLocation.y);
+    context.lineTo(position.x, position.y);
     context.stroke();
-  }
-  event.preventDefault();
 }
 
-function stop(event) {
-  if (is_drawing) {
-    context.stroke();
-    context.closePath();
-    is_drawing = false;
-  }
-  event.preventDefault();
-  restore_array.push(context.getImageData(0, 0, canvas.width, canvas.height));
-  start_index += 1;
+function drawCircle(position) {
+    var radius = Math.sqrt(Math.pow((dragStartLocation.x - position.x), 2) + Math.pow((dragStartLocation.y - position.y), 2));
+    context.beginPath();
+    context.arc(dragStartLocation.x, dragStartLocation.y, radius, 0, 2 * Math.PI, false);
 }
 
-function getX(event) {
-  if (event.pageX == undefined) {return event.targetTouches[0].pageX - canvas.offsetLeft}
-  else {return event.pageX - canvas.offsetLeft}
-  }
+function drawPolygon(position, sides, angle) {
+    var coordinates = [],
+        radius = Math.sqrt(Math.pow((dragStartLocation.x - position.x), 2) + Math.pow((dragStartLocation.y - position.y), 2)),
+        index = 0;
 
-
-function getY(event) {
-  if (event.pageY == undefined) {return event.targetTouches[0].pageY - canvas.offsetTop}
-  else {return event.pageY - canvas.offsetTop}
-}
-
-canvas.addEventListener("touchstart", start, false);
-canvas.addEventListener("touchmove", draw, false);
-canvas.addEventListener("touchend", stop, false);
-canvas.addEventListener("mousedown", start, false);
-canvas.addEventListener("mousemove", draw, false);
-canvas.addEventListener("mouseup", stop, false);
-canvas.addEventListener("mouseout", stop, false);
-
-function Restore() {
-  if (start_index <= 0) {
-    Clear()
-  } else {
-    start_index += -1;
-    restore_array.pop();
-    if ( event.type != 'mouseout' ) {
-      context.putImageData(restore_array[start_index], 0, 0);
+    for (index = 0; index < sides; index++) {
+        coordinates.push({x: dragStartLocation.x + radius * Math.cos(angle), y: dragStartLocation.y - radius * Math.sin(angle)});
+        angle += (2 * Math.PI) / sides;
     }
-  }
+
+    context.beginPath();
+    context.moveTo(coordinates[0].x, coordinates[0].y);
+    for (index = 1; index < sides; index++) {
+        context.lineTo(coordinates[index].x, coordinates[index].y);
+    }
+
+    context.closePath();
 }
 
-function Clear() {
-    context.fillStyle = "white";
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    context.fillRect(0, 0, canvas.width, canvas.height);
-    restore_array = [];
-    start_index = -1;
+function draw(position) {
+
+    var fillBox = document.getElementById("fillBox"),
+        shape = document.querySelector('input[type="radio"][name="shape"]:checked').value,
+        polygonSides = document.getElementById("polygonSides").value,
+        polygonAngle = document.getElementById("polygonAngle").value,
+        lineCap = document.querySelector('input[type="radio"][name="lineCap"]:checked').value;
+
+    context.lineCap = lineCap;
+
+    if (shape === "circle") {
+        drawCircle(position);
+    }
+    if (shape === "line") {
+        drawLine(position);
+    }
+
+    if (shape === "polygon") {
+        drawPolygon(position, polygonSides, polygonAngle * (Math.PI / 180));
+    }
+    if (fillBox.checked) {
+        context.fill();
+    } else {
+        context.stroke();
+    }
 }
+
+function dragStart(event) {
+    dragging = true;
+    dragStartLocation = getCanvasCoordinates(event);
+    takeSnapshot();
+}
+
+function drag(event) {
+    var position;
+    if (dragging === true) {
+        restoreSnapshot();
+        position = getCanvasCoordinates(event);
+        draw(position, "polygon");
+    }
+}
+
+function dragStop(event) {
+    dragging = false;
+    restoreSnapshot();
+    var position = getCanvasCoordinates(event);
+    draw(position, "polygon");
+}
+
+function changeLineWidth() {
+    context.lineWidth = this.value;
+    event.stopPropagation();
+}
+
+function changeFillStyle() {
+    context.fillStyle = this.value;
+    event.stopPropagation();
+}
+
+function changeStrokeStyle() {
+    context.strokeStyle = this.value;
+    event.stopPropagation();
+}
+
+function changeBackgroundColor() {
+    context.save();
+    context.fillStyle = document.getElementById("backgroundColor").value;
+    context.fillRect(0, 0, canvas.width, canvas.height);
+    context.restore();
+}
+
+function eraseCanvas() {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+function init() {
+    canvas = document.getElementById("canvas");
+    context = canvas.getContext('2d');
+    var lineWidth = document.getElementById("lineWidth"),
+        fillColor = document.getElementById("fillColor"),
+        strokeColor = document.getElementById("strokeColor"),
+        canvasColor = document.getElementById("backgroundColor"),
+        clearCanvas = document.getElementById("clearCanvas");
+
+    context.strokeStyle = strokeColor.value;
+    context.fillStyle = fillColor.value;
+    context.lineWidth = lineWidth.value;
+
+
+    canvas.addEventListener('mousedown', dragStart, false);
+    canvas.addEventListener('mousemove', drag, false);
+    canvas.addEventListener('mouseup', dragStop, false);
+    lineWidth.addEventListener("input", changeLineWidth, false);
+    fillColor.addEventListener("input", changeFillStyle, false);
+    strokeColor.addEventListener("input", changeStrokeStyle, false);
+    canvasColor.addEventListener("input", changeBackgroundColor, false);
+    clearCanvas.addEventListener("click", eraseCanvas, false);
+}
+
+window.addEventListener('load', init, false);
